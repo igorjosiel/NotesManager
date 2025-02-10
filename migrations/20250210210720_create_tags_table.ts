e27c7580073c44d96f5337f5feb1bff3bded1307
Igor Josiel <igorjosielvitalino@gmail.com>
2025-02-10 18:10:49 -0300

    #feat: Cria a migration de tags.

---
+++ migrations/20250210210720_create_tags_table.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('tags', (table) => {
+    table.uuid('id').primary();
+    table.string('name', 100).notNullable().unique();
+    table.text('description').nullable();
+    table.boolean('is_active').defaultTo(true);
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('tags');
+}
